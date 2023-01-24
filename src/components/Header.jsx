332bfa4d278e80eb8781f311809bb321a066e07c
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-25 00:25:11 +0530

    minor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ const Header = (props) => {
   return (
     <div className='relative bg-teal-500 p-8 rounded-lg'>
         <h1 className='text-4xl md:text-6xl font-bold text-white  text-center'>Image Gallery</h1>
-        <h3 onClick={props.toggleDarkMode} className='absolute top-2 right-3 bg-white p-1 rounded font-bold cursor-pointer'>
-          {props.darkMode ? "Light Mode" : "Dark Mode"  }
+        <h3 onClick={props.toggleDarkMode} className='absolute top-1 right-1 bg-white py-1 px-2 rounded font-bold cursor-pointer'>
+          {props.darkMode ? "Light" : "Dark"  }
         </h3>
     </div>
   )
