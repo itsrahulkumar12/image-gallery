79c4d9817be35b4bd9aadde54221707f8c75d800
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-24 19:42:44 +0530

    made responsive

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -25,7 +25,7 @@ const ImageCard = ({image}) => {
           </li>
         </ul>
       </div>
-      <div className="px-6 py-4">
+      <div className=" flex items-center flex-wrap gap-1  px-6 py-4">
         {tags.map((tag, index) => 
         <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
           #{tag}
