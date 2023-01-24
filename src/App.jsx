9ea77cbbffbc7d42dcbb3f525c3157f6be00ed59
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-24 22:45:54 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ImageSearch from "./components/ImageSearch";
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("mountains");
 
   useEffect(() => {
     fetch(
@@ -23,8 +23,10 @@ function App() {
   return (
     <div className="container mx-auto p-5">
       <ImageSearch searchText={text => setTerm(text)} />
+
+      {!isLoading && images.length == 0 && <h1 className="text-5xl text-center font-semibold text-red-600 mx-auto mt-32">No Images Found !! </h1>}
       
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      {isLoading ? <h1 className="text-6xl text-center text-teal-500 mx-auto mt-32">Loading...</h1> : <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {images.map((image) => {
           return <ImageCard key={image.id} image={image} />;
         })}
