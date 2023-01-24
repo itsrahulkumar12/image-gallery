2cd5a3970da86506943624e69550fd090243756b
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-25 00:37:49 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("mountains");
-  const [darkMode,setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     fetch(
@@ -23,23 +23,33 @@ function App() {
   }, [term]);
 
   const toggleDarkMode = () => {
-      setDarkMode(prevDarkMode => !prevDarkMode)
-  }
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
 
   return (
-    <div className={darkMode ? "dark" : ""}>
+    <div className={darkMode ? "dark h-[100vh]" : ""}>
       <div className="container mx-auto p-5">
-      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <ImageSearch searchText={text => setTerm(text)} />
+        <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <ImageSearch searchText={(text) => setTerm(text)} />
 
-      {!isLoading && images.length == 0 && <h1 className="text-5xl text-center font-semibold text-red-600 mx-auto mt-32">No Images Found !! </h1>}
-      
-      {isLoading ? <h1 className="text-6xl text-center text-teal-500 mx-auto mt-32">Loading...</h1> : <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-        {images.map((image) => {
-          return <ImageCard key={image.id} image={image} />;
-        })}
-      </div>}
-    </div>
+          {!isLoading && images.length == 0 && (
+            <h1 className="text-5xl text-center font-semibold text-red-600 mx-auto mt-32">
+              No Images Found !!
+            </h1>
+          )}
+        
+        {isLoading ? (
+          <h1 className="text-6xl text-center text-teal-500 mx-auto mt-32">
+            Loading...
+          </h1>
+        ) : (
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {images.map((image) => {
+              return <ImageCard key={image.id} image={image} />;
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
