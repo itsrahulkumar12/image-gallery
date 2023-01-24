d2b5287b33b00d45ada70a4a426a67f67797d1d5
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-24 19:25:05 +0530

    search added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -17,11 +18,13 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto p-5">
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+      <ImageSearch searchText={text => setTerm(text)} />
+      
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid  grid-cols-3  gap-4">
         {images.map((image) => {
           return <ImageCard key={image.id} image={image} />;
         })}
