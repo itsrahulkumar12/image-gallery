f9d43b90cd33cd18152a15cec9ec2d7502411a85
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-24 21:50:04 +0530

    minor

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -6,12 +6,13 @@ const ImageSearch = ({searchText}) => {
     const onSubmit = (e) => {
         e.preventDefault();
         searchText(text);
+        
     }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+    <div className="md:max-w-sm rounded overflow-hidden my-10 mx-auto">
       <form onSubmit={onSubmit} className="w-full max-w-sm">
-        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
           <input
             onChange={e => setText(e.target.value)}
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
